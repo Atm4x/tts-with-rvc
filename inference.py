a565e84d9c18bad73e1eb6d4c93294122c468688
SunSual <mrtaktplay@gmail.com>
2023-12-19 18:12:12 +0300

    first commit

---
+++ inference.py
@@ -0,0 +1,67 @@
+from rvc_infer import rvc_convert
+
+import os
+import edge_tts as tts
+from edge_tts import VoicesManager
+import asyncio, concurrent.futures
+import gradio as gr
+from rvc_infer import rvc_convert
+import config
+import hashlib
+from datetime import datetime
+
+class TTS_RVC:
+    def __init__(self, rvc_path, model_path, voice_path="ru-RU-DmitryNeural"):
+        if not os.path.exists('input'):
+            os.mkdir('input')
+        if not os.path.exists('output'):
+            os.mkdir('output')
+
+        self.pool = concurrent.futures.ThreadPoolExecutor()
+        self.current_voice = ""
+        self.can_speak = True
+        self.current_model = ""
+        self.rvc_path = rvc_path
+
+    def set_voice(self, voice):
+        self.current_voice = voice
+
+    def get_voices(self):
+        return get_voices()
+
+    def __call__(self, text, pitch):
+        if not self.can_speak:
+            print("TTS is busy.")
+            return None
+        self.can_speak = False
+        path = self.pool.submit(asyncio.run, speech(text, pitch, self.current_voice)).result()
+        self.can_speak = True
+        return path
+
+
+
+def date_to_short_hash():
+    current_date = datetime.now()
+    date_str = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    sha256_hash = hashlib.sha256(date_str.encode()).hexdigest()
+    short_hash = sha256_hash[:8]
+    return short_hash
+
+
+async def get_voices():
+    voicesobj = await VoicesManager.create()
+    return [data["ShortName"] for data in voicesobj.voices]
+
+async def speech(model_path, input_path, rvc_path, message, pitch=0, voice="ru-RU-DmitryNeural"):
+    communicate = tts.Communicate(message, voice)
+    file_name = "test"
+    await communicate.save("input\\" + file_name)
+    output_path = rvc_convert(model_path=model_path,
+                              input_path=input_path,
+                              rvc_path=rvc_path,
+                              f0_up_key=pitch)
+    name = date_to_short_hash()
+    os.rename("output\\out.wav", "output\\" + name + ".wav")
+    os.remove("input\\" + file_name)
+    return "output\\" + name + ".wav"
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tts-with-rvc',
+    version='0.1',
+    description='tts with rvc pipeline',
+    author='SunSual',
+    packages=find_packages(),
+)

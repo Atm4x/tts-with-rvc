358f4a47c538efd75fe9169f4f680d10511592a5
SunSual <mrtaktplay@gmail.com>
2023-12-19 18:56:44 +0300

    setup change

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='tts-with-rvc',
-    version='0.1',
-    description='tts with rvc pipeline',
+    version='0.1.0',
+    description='TTC with RVC pipeline',
     author='SunSual',
     packages=find_packages(),
 )

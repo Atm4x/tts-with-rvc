907b57dd9dc25726d22c8720ae22d5a218bccf75
SunSual <mrtaktplay@gmail.com>
2023-12-19 19:10:24 +0300

    setup change

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='tts-with-rvc',
-    version='0.1.0',
+    version='0.1.1',
     description='TTC with RVC pipeline',
     author='SunSual',
-    packages=[find_packages(), 
-              "edge-tts", 
-              "huggingface_hub"
-              ],
+    packages=find_packages(),
+    install_requires=[
+        "huggingface_hub",
+        "torch",
+        "torchaudio",
+        "edge-tts"
+    ]
 )

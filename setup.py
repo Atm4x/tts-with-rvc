c538ad3bc0c90c2a6874a788a7cec50554ef5a0b
Artem <96025052+Atm4x@users.noreply.github.com>
2024-04-22 22:05:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version='0.1.2',
     description='TTS with RVC pipeline',
     author='Atm4x',
-    package_dir={'': 'src'},  # Include this line to specify the source directory
     packages=find_packages(),  # Update find_packages to look in 'src' director
     install_requires=[
         "huggingface_hub",

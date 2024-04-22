6a02bd9254c431977f3835d973cad466562af0db
Artem <96025052+Atm4x@users.noreply.github.com>
2024-04-22 23:14:57 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup, find_packages
-import os
-
 
 setup(
     name='tts_with_rvc',
     version='0.1.2',
     description='TTS with RVC pipeline',
     author='Atm4x',
-    packages=find_packages(include=['inference', 'inference.*']),  # Update find_packages to look in 'src' director
+    packages=find_packages(),  # Update find_packages to look in 'src' director
     install_requires=[
         "huggingface_hub",
         "torch",

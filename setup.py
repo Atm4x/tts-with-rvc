d2ca6fc46e5ccdfa959af85e9d3065b513e9aedd
Artem <96025052+Atm4x@users.noreply.github.com>
2024-04-22 22:58:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.1.2',
     description='TTS with RVC pipeline',
     author='Atm4x',
-    packages=find_packages(),  # Update find_packages to look in 'src' director
+    packages=find_packages(include=['tts_with_rvc', 'tts_with_rvc.*']),  # Update find_packages to look in 'src' director
     install_requires=[
         "huggingface_hub",
         "torch",
